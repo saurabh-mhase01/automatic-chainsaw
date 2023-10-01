56b76907494cffa8a2fccf7ff6057daa14b3ed20
Saurabh Gokul Mhase <75540568+saurabh-mhase01@users.noreply.github.com>
2023-10-01 22:44:34 +0530

    dayone

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
-i
+#include<iostream>
+using namespace std;
+class Date {
+    int day,month,year;
+    Date(int day,int month,int year){
+        this->day= day;
+        this->month = month;
+        this->year= year;
+
+    }
+    int getDay(){
+        return this->day;
+    }
+    int getMonth(){
+        return this->month;
+    }
+    int getYear(){
+        return this->year;
+    }
+}
+class transactions {
+    int amount;
+    string transactions_id;
+    
+}
+int main(){
+
+    cout<<"hello world ";
+
+}
